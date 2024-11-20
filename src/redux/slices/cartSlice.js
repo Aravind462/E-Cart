db14e495ed9ea1f00b0931655fe4bfb5e04cd522
Aravind462 <aravindas2003@gmail.com>
2024-11-20 10:04:00 +0530

    cart completed

--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -4,7 +4,7 @@ const cartSlice = createSlice({
     name:'cartItems',
     initialState:[],
     reducers:{
-        addToCart:(state,actionByComponent)=>{
+        addToCart : (state,actionByComponent)=>{
             const existingProduct = state.find(item=>item.id==actionByComponent.payload.id);
             if(existingProduct){
                 existingProduct.quantity++
@@ -14,9 +14,29 @@ const cartSlice = createSlice({
             }else{
                 state.push({...actionByComponent.payload,quantity:1,totalPrice:actionByComponent.payload.price})
             }
+        },
+        incrementQuantity : (state,actionByCart)=>{
+            const existingProduct = state.find(item=>item.id==actionByCart.payload)
+            existingProduct.quantity++
+            existingProduct.totalPrice = existingProduct.quantity * existingProduct.price
+            const remainingProducts = state.filter(item=>item.id!=existingProduct.id)
+            state = [...remainingProducts,existingProduct]
+        },
+        removeCartItem : (state,actionByCart)=>{
+            return state.filter(item=>item.id!=actionByCart.payload)
+        },
+        decrementQuantity : (state,actionByCart)=>{
+            const existingProduct = state.find(item=>item.id==actionByCart.payload)
+            existingProduct.quantity--
+            existingProduct.totalPrice = existingProduct.quantity * existingProduct.price
+            const remainingProducts = state.filter(item=>item.id!=existingProduct.id)
+            state = [...remainingProducts,existingProduct]
+        },
+        emptyCart : (state)=>{
+            return state = []
         }
     }
 })
 
-export const { addToCart } = cartSlice.actions
+export const { addToCart,incrementQuantity,removeCartItem,decrementQuantity,emptyCart } = cartSlice.actions
 export default cartSlice.reducer
